abcae811afd10186f6fe290862fb60b687382cf5
blue-hope <ms7045436@likelion.org>
2019-06-27 12:56:40 +0900

    thread handling

--- proxy.py
+++ proxy.py
@@ -35,8 +35,53 @@ def main():
 
 def proxy_thread(conn, client_addr):
     request = conn.recv(MAX_DATA_RECY)
-    first_line = request.split('n')[0]
-    print("line: ", first_line)
+    first_line = request.decode().split('n')[0]
+    url = first_line.split(' ')[1]
+
+    if(DEBUG):
+        print("line: ", first_line)
+        print("url: ", url)
+    
+    http_pos = url.find('://')
+    if(http_pos == -1):
+        temp = url
+    else
+        temp = url[(http_pos+3):]
+    
+    port_pos = temp.find(":")
+
+    webserver = ""
+    port = -1
+    if(port_pos == -1 or webserver_pos < port_pos):
+        port = 80
+        webserver = temp[:webserver_pos]
+    else:
+        port = int((temp[(port_pos+1):])[:webserver_pos-port_pos-1])
+        webserver = temp[:port_pos]
+
+    print("connect: ", webserver, port)
+
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((webserver, port))
+        s.send(request)
+
+        while 1:
+            data = s.recv(MAX_DATA_RECY)
+
+            if(len(data) > 0):
+                conn.send(data)
+            else:
+                break
+        s.close()
+        conn.close()
+    except OSError as e:
+        if s:
+            s.close()
+        if conn:
+            conn.close()
+        print("runtime error: ", e)
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
