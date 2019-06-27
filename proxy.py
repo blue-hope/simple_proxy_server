49d6f7c938035a5e6195a09577e60a18a3aaea9b
blue-hope <ms7045436@likelion.org>
2019-06-27 13:02:35 +0900

    typo

--- proxy.py
+++ proxy.py
@@ -45,12 +45,15 @@ def proxy_thread(conn, client_addr):
     http_pos = url.find('://')
     if(http_pos == -1):
         temp = url
-    else:
+    else
         temp = url[(http_pos+3):]
     
     port_pos = temp.find(":")
 
-
+    webserver_pos = temp.find("/")
+    if webserver_pos == -1:
+        webserver_pos = len(temp)
+        
     webserver = ""
     port = -1
     if(port_pos == -1 or webserver_pos < port_pos):
